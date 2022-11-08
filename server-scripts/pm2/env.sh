29d76b04ee14087b4581b3c5672bea91ac623761
pbvahlst <peter@vahlstrup.com>
2022-11-08 15:54:41 +0100

    fix some more bugs

--- server-scripts/pm2/env.sh
+++ server-scripts/pm2/env.sh
@@ -10,9 +10,10 @@ export USER_WHITELIST_MAX_JOBS="1" # How many jobs can a whitelisted user have r
 export USER_EMAIL_WHITELIST_FILE_PATH="" # The path for a newline delimited list of whitelisted user emails
 export MAIL_SMTP_HOST="" # [REQUIRED] SMTP-server hostname (e.g. `smtp.example.com`)
 export MAIL_SMTP_PORT="" # [REQUIRED] SMTP-server port
-export MAIL_SMTP_USER="" # [REQUIRED] SMTP-server username (most likely the email address)
-export MAIL_SMTP_PASS="" # [REQUIRED] SMTP-server password (most likely password for the email address)
+export MAIL_SMTP_USER="" # SMTP-server username (most likely the email address)
+export MAIL_SMTP_PASS="" # SMTP-server password (most likely password for the email address)
 export MAIL_MESSAGE_FROM="" # [REQUIRED] the email to set as the `from` field (e.g. `info@example.com` or `"John Doe" <info@example.com>`)
+export ADMIN_ACCESS_SECRET="" # [REQUIRED] a secret code for logging into the admin panel must be >= 8 characters long
 export MAIL_SMTP_DISABLE_VERIFICATION="false" # set to `true` disable the verification check when server starts, disable this during development for faster restart times
 export MAIL_DISABLED="false" # set to `true` disable sending emails
 export PORT="3000" # the port the server should use
